0810f27d6d225c5afd694cce762c3fd47725d3dd
itbortnik <itbortnik@gmail.com>
2024-12-16 21:39:20 +0300

    third

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import sys
 import random
-from PyQt6 import QtWidgets, QtGui, QtCore
 from ui_mainwindow import Ui_MainWindow
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.setText("Добавить окружность")
+        self.verticalLayout.addWidget(self.pushButton)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        MainWindow.setWindowTitle("Пример")
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -9,7 +32,6 @@ class MainWindow(QtWidgets.QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.resize(800, 600)
 
         self.ui.pushButton.clicked.connect(self.add_circle)
         self.circles = []
@@ -20,16 +42,17 @@ class MainWindow(QtWidgets.QMainWindow):
         h = self.ui.centralwidget.height()
         x = random.randint(diameter // 2, w - diameter // 2)
         y = random.randint(diameter // 2, h - diameter // 2)
-        self.circles.append((x, y, diameter))
+        color = QtGui.QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.circles.append((x, y, diameter, color))
         self.update()
 
     def paintEvent(self, event):
         super().paintEvent(event)
         painter = QtGui.QPainter(self)
         painter.setRenderHint(QtGui.QPainter.RenderHint.Antialiasing)
-        painter.setPen(QtCore.Qt.PenStyle.NoPen)
-        painter.setBrush(QtGui.QColor('yellow'))
-        for (x, y, d) in self.circles:
+        for (x, y, d, color) in self.circles:
+            painter.setPen(QtCore.Qt.PenStyle.NoPen)
+            painter.setBrush(color)
             painter.drawEllipse(QtCore.QPointF(x, y), d / 2.0, d / 2.0)
 
 
